4b1e1ec6a18f3a482eded1ca5308a63f8084e4e2
Nicolas Aoki <nicolasaoki@alunos.utfpr.edu.br>
2018-01-10 01:34:20 -0200

    alteracoes shared

--- testaArquivo.py
+++ testaArquivo.py
@@ -1,17 +1,9 @@
-import pybedtools
-from pybedtools import BedTool
-from pybedtools import Interval
 import sys
 import glob, os
 
-def show_value(s): #retira informacoes dos arquivos .gff e .fasta
-    if sys.version_info.major == 2:
-        if (isinstance(s, unicode)):
-            return str(s)
-    return s
 
 def org_sequencia(s,comeco,fim):
-    arquivo = "/home/nicolas/Desktop/streptoRNA/arquivos/Organismos/"+s+".fasta"
+    arquivo = "/Users/Nicolas/Desktop/streptoRNA/arquivos/Organismos/"+s+".fasta"
     nova_string = ''
     with open(arquivo) as f:
         lines = f.readlines()
@@ -22,23 +14,21 @@ def org_sequencia(s,comeco,fim):
     return nova_string
 
 def main():
-    pasta = "/home/nicolas/Desktop/streptoRNA/arquivos/sRNAs_annotations/"
-    with open("insert_feature.sql", 'r') as f:
-        for file in os.listdir(pasta):
-            if file.endswith("final.gff"):  # se o arquivo termina com .gff
-                i = BedTool(pasta + file)
-                # acrescenta na string sRNAs com o arquivo .gff
-                for k, a in enumerate(i):  # lista todas as linhas de i(arquivo) com indice a, e k sendo cada arquivo
-                    start = show_value(a['start'])
-                    nome = a[2]
-                    fim = show_value(a['stop'])
-                    nome_chrom = show_value(a['chrom'])
-                    feature_name = show_value(a['name'])
-                    organism_id = ("(SELECT o.organism_id FROM organism as o WHERE abbreviation like '"+nome_chrom+"')")
-                    publication_id = 1
-                    strand = show_value(a['strand'])
-                    print nome
-        f.close()
+    pasta = "/Users/Nicolas/Desktop/streptoRNA/arquivos/regions_annotations/"
+    for file in os.listdir(pasta):
+        if file.startswith("EXCLUSIVE"):
+            with open(pasta + file, 'r') as g:
+                linhas = g.readlines()
+                for line in linhas:
+                    lista = line.split()
+                    loc_identification = lista[2]
+                    host_gene =  lista[0]
+                    start = int(lista[3])
+                    end = int(lista[4])
+                    sequence = org_sequencia(host_gene, start, end)
+                    strand = "+"
+                    print("loc_identification : {}, host : {} , start : {}, fim : {} : sequence : {}".format(loc_identification,host_gene,start,end,sequence))
+
 
 if __name__ == '__main__':
     main()
--- testes_codigos.py
+++ testes_codigos.py
@@ -1,19 +1,11 @@
-import pybedtools
-from pybedtools import BedTool
-from pybedtools import Interval
 import sys
 import glob, os
 import time
 
-def show_value(s): #retira informacoes dos arquivos .gff e .fasta
-    if sys.version_info.major == 2:
-        if (isinstance(s, unicode)):
-            return str(s)
-    return s
 
 #retira a sequencia desejada do organismo
 def org_sequencia(s,comeco,fim):
-    arquivo = "/home/nicolas/Desktop/streptoRNA/arquivos/Organismos/"+s+".fasta"
+    arquivo = "/Users/Nicolas/Desktop/streptoRNA/arquivos/Organismos/"+s+".fasta"
     nova_string = ''
     with open(arquivo) as f:
         lines = f.readlines()
@@ -73,23 +65,27 @@ def insere_tabela_feature(pasta):  #tabela feature final.gff
     with open("insert_feature.sql", 'w') as f:
         for file in os.listdir(pasta):
             if file.endswith("final.gff"):  # se o arquivo termina com .gff
-                i = BedTool(pasta + file)  # acrescenta na string sRNAs com o arquivo .gff
-                for k, a in enumerate(i):  # lista todas as linhas de i(arquivo) com indice a, e k sendo cada arquivo
-                    start = show_value(a['start'])
-                    fim = show_value(a['stop'])
-                    nome_chrom = show_value(a['chrom'])
-                    feature_name = show_value(a['name'])
-                    nome_feature = a[2]
-                    organism_id = ("(SELECT o.organism_id FROM organism as o WHERE abbreviation like '"+nome_chrom+"')")
-                    publication_id = 1
-                    strand = show_value(a['strand'])
-                    f.write("INSERT INTO feature (organism_id,publication_id,start,end,strand,sequence,feature_name) \n")  # preenche tabela feature
-                    f.write("VALUES({},{},{},{},{},{},{});\n".format(organism_id,publication_id, start, fim, "'" + strand + "'",
-                                                                            "'" + org_sequencia(nome_chrom, start,fim)+"'", "'" + feature_name + "'"))
-                    f.write("INSERT INTO feature_analysis_result (id_feature,id_analysis_type)\n")
-                    f.write("VALUES ((SELECT MAX(feature_id) FROM feature),0);\n")
-                    f.write("INSERT INTO feature_type (feature_id,type_id)\n")
-                    f.write("VALUES ((SELECT MAX(feature_id) FROM feature),3);\n")
+               with open(pasta+file, 'r') as g:
+                    linhas = g.readlines()
+                    for line in linhas:
+                        lista = line.split()
+                        start = int(lista[3])
+                        fim = int(lista[4])
+                        nome_chrom = lista[0]
+                        feature_name = lista[8]
+                        feature_name = feature_name[16:]
+                        index = feature_name.index(';')
+                        feature_name = feature_name[:index]
+                        strand = lista[6]
+                        organism_id = ("(SELECT o.organism_id FROM organism as o WHERE abbreviation like '"+nome_chrom+"')")
+                        publication_id = 1
+                        f.write("INSERT INTO feature (organism_id,publication_id,start,end,strand,sequence,feature_name) \n")  # preenche tabela feature
+                        f.write("VALUES({},{},{},{},{},{},{});\n".format(organism_id,publication_id, start, fim, "'" + strand + "'","'" + org_sequencia(nome_chrom, start,fim)+"'", "'" + feature_name + "'"))
+                        f.write("INSERT INTO feature_analysis_result (id_feature,id_analysis_type)\n")
+                        f.write("VALUES ((SELECT MAX(feature_id) FROM feature),0);\n")
+                        f.write("INSERT INTO feature_type (feature_id,type_id)\n")
+                        f.write("VALUES ((SELECT MAX(feature_id) FROM feature),3);\n")
+                    g.close()
         f.close()
 
 def insere_localization_shared(pasta):
@@ -99,10 +95,10 @@ def insere_localization_shared(pasta):
                 with open(pasta+file, 'r') as g:
                     linhas = g.readlines
 
-def insere_localization(pasta):
-    with open("insere_localization_teste.sql", 'w') as f:
+def insere_localization_exclusive(pasta):
+    with open("insere_localization_exclusive.sql", 'w') as f:
         for file in os.listdir(pasta):
-            if file.startswith("CORE") or file.startswith("EXCLUSIVE"):
+            if file.startswith("EXCLUSIVE"):
                 with open(pasta + file, 'r') as g:
                     linhas = g.readlines()
                     for line in linhas:
@@ -112,26 +108,46 @@ def insere_localization(pasta):
                         start = int(lista[3])
                         end = int(lista[4])
                         sequence = org_sequencia(host_gene, start, end)
-                        strand = "+"
                         feature_id = "(SELECT f.feature_id FROM feature f,localization l WHERE f.start > l.start and f.end < l.end)"
                         f.write("INSERT INTO localization (loc_identification,host_gene,sequence,start,end,strand)\n")
-                        f.write("VALUES({},{},{},{},{},{});\n".format("'"+loc_identification+"'","'"+host_gene+"'","'"+sequence+"'",start,end,"'"+strand+"'"))
-                        f.write("#insercao na tabela associativa\n")
+                        f.write("VALUES({},{},{},{},{});\n".format("'"+loc_identification+"'","'"+host_gene+"'","'"+sequence+"'",start,end))
                         f.write("INSERT INTO featureloc (loc_id,feature_id)\n")
                         f.write("VALUES ((SELECT MAX(loc_id) FROM localization),"+feature_id+");\n")
-
+                    g.close()
+        f.close()
+def insere_localization_core(pasta):
+    with open("insere_localization_core.sql", 'w') as f:
+        for file in os.listdir(pasta):
+            if file.startswith("CORE"):
+                with open(pasta + file, 'r') as g:
+                    linhas = g.readlines()
+                    for line in linhas:
+                        lista = line.split()
+                        loc_identification = lista[2]
+                        host_gene =  lista[0]
+                        start = int(lista[3])
+                        end = int(lista[4])
+                        sequence = org_sequencia(host_gene, start, end)
+                        feature_id = "(SELECT f.feature_id FROM feature f,localization l WHERE f.start > l.start and f.end < l.end)"
+                        f.write("INSERT INTO localization (loc_identification,host_gene,sequence,start,end,strand)\n")
+                        f.write("VALUES({},{},{},{},{});\n".format("'"+loc_identification+"'","'"+host_gene+"'","'"+sequence+"'",start,end))
+                        f.write("INSERT INTO featureloc (loc_id,feature_id)\n")
+                        f.write("VALUES ((SELECT MAX(loc_id) FROM localization),"+feature_id+");\n")
+                    g.close()
+        f.close()
 def main():
     #Pastas
-    organismos ="/home/nicolas/Desktop/streptoRNA/arquivos/Organismos/"
-    sRNAs_annotations ="/home/nicolas/Desktop/streptoRNA/arquivos/sRNAs_annotations/"
-    hgt_regions ="/home/nicolas/Desktop/streptoRNA/arquivos/HGT_regions/"
-    regions_annotations ="/home/nicolas/Desktop/streptoRNA/arquivos/regions_annotations/"
+    organismos ="/Users/Nicolas/Desktop/streptoRNA/arquivos/Organismos/"
+    sRNAs_annotations ="/Users/Nicolas/Desktop/streptoRNA/arquivos/sRNAs_annotations/"
+    hgt_regions ="/Users/Nicolas/Desktop/streptoRNA/arquivos/HGT_regions/"
+    regions_annotations ="/Users/Nicolas/Desktop/streptoRNA/arquivos/regions_annotations/"
     #inserts
     insere_tabela_organism(organismos)
     insere_tabela_publication("0","Annotation and distribution of ncRNA families in genomes of Streptococcus agalactiae","2018","UTFPR")
     insere_tabela_feature(sRNAs_annotations)
     insere_tabela_analysis_type()
     insere_tabela_type()
-    insere_localization(regions_annotations)
+    insere_localization_core(regions_annotations)
+    insere_localization_exclusive(regions_annotations)
 if __name__ == '__main__':
     main()
