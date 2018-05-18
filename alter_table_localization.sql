5de3b9ba5abbcca496aa667f6067b15e2f508e1c
Nicolas Aoki <nicolasaoki@alunos.utfpr.edu.br>
2018-05-18 06:16:00 -0300

    alter table localization e tabela.xls atualizada

---
+++ alter_table_localization.sql
@@ -0,0 +1,82 @@
+UPDATE localization
+SET organism_id = 1
+WHERE host_gene like 'AE009948';
+UPDATE localization
+SET organism_id = 2
+WHERE host_gene like 'AL732656';
+UPDATE localization
+SET organism_id = 3
+WHERE host_gene like 'CP000114';
+UPDATE localization
+SET organism_id = 4
+WHERE host_gene like 'CP003810';
+UPDATE localization
+SET organism_id = 5
+WHERE host_gene like 'CP003919';
+UPDATE localization
+SET organism_id = 6
+WHERE host_gene like 'CP006910';
+UPDATE localization
+SET organism_id = 7
+WHERE host_gene like 'CP007482';
+UPDATE localization
+SET organism_id = 8
+WHERE host_gene like 'CP007565';
+UPDATE localization
+SET organism_id = 9
+WHERE host_gene like 'CP007570';
+UPDATE localization
+SET organism_id = 10
+WHERE host_gene like 'CP007571';
+UPDATE localization
+SET organism_id = 11
+WHERE host_gene like 'CP007572';
+UPDATE localization
+SET organism_id = 12
+WHERE host_gene like 'CP007631';
+UPDATE localization
+SET organism_id = 13
+WHERE host_gene like 'CP007632';
+UPDATE localization
+SET organism_id = 14
+WHERE host_gene like 'CP010319';
+UPDATE localization
+SET organism_id = 15
+WHERE host_gene like 'CP010867';
+UPDATE localization
+SET organism_id = 16
+WHERE host_gene like 'CP011325';
+UPDATE localization
+SET organism_id = 17
+WHERE host_gene like 'CP011326';
+UPDATE localization
+SET organism_id = 18
+WHERE host_gene like 'CP011327';
+UPDATE localization
+SET organism_id = 19
+WHERE host_gene like 'CP011328';
+UPDATE localization
+SET organism_id = 20
+WHERE host_gene like 'CP011329';
+UPDATE localization
+SET organism_id = 21
+WHERE host_gene like 'CP012419';
+UPDATE localization
+SET organism_id = 22
+WHERE host_gene like 'CP013202';
+UPDATE localization
+SET organism_id = 23
+WHERE host_gene like 'FO393392';
+UPDATE localization
+SET organism_id = 24
+WHERE host_gene like 'HF952104';
+UPDATE localization
+SET organism_id = 25
+WHERE host_gene like 'HF952105';
+UPDATE localization
+SET organism_id = 26
+WHERE host_gene like 'HF952106';
+UPDATE localization
+SET organism_id = 27
+WHERE host_gene like 'HG939456';
+
